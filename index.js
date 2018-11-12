b146f3ac75745c939e968f8d6da39398941b4845
katya-shprd <shepardkate13@gmail.com>
2018-11-12 13:15:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('Worked !!!!')
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log(`Server started at TCP ${port}`)
+
+})
+
